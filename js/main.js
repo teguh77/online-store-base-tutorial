6c329272e5d29de5ea5da81496561a3f493ef5c1
Tama <teguhpratama337@gmail.com>
2019-08-26 07:59:31 +0700

    part 6 initial

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$('.open-menu-icon').click(function (){
+  $('.main-nav-outer').addClass('open-menu');
+});
+$('.menu-close-icon').click(function (){
+  $('.main-nav-outer').removeClass('open-menu');
+});
